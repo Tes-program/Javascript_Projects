1b9f38c86016829498df367e9f0eaf3018fa36c6
Tes-program <teslimodumuyiwa@gmail.com>
2022-04-25 08:18:08 +0100

    change can be intoxicating

--- Easy/Weather_App/script.js
+++ Easy/Weather_App/script.js
@@ -1,1 +1,6 @@
-feather.replace()
+var inputText =  document.querySelector('#search')
+var submit = document.querySelector('#submit')
+var city = document.querySelector('#location')
+var temperature = document.querySelector('#temperature')
+var desc = document.querySelector('#desc')
+
---
+++ Easy/Weather_App/server.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const ApiKey = process.env.OPEN_WEATHER_API_KEY;
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.post('/weather', (req, res) => { 
+
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
