2a1a9a51e1f1385394c29fea1e460f70788601bb
Tes-program <teslimodumuyiwa@gmail.com>
2022-04-25 12:46:40 +0100

    Node js

--- Easy/Weather_App/script.js
+++ Easy/Weather_App/script.js
@@ -1,6 +1,22 @@
-var inputText =  document.querySelector('#search')
-var submit = document.querySelector('#submit')
-var city = document.querySelector('#location')
-var temperature = document.querySelector('#temperature')
-var desc = document.querySelector('#desc')
-
+const input = document.querySelector('[data-search-bar]');
+const searchBox = new google.maps.places.SearchBox(input)
+searchBox.addListener('places_changed', () => {
+    const place = searchBox.getPlaces()[0];
+    if (place == null) return 
+    const latitude = place.geometry.location.lat();
+    const longitude = place.geometry.location.lng();
+    fetch ('/weather' , {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            latitude: latitude,
+            longitude: longitude
+        })
+    })
+    .then(response => response.json()).then(data => {
+        setWeatherData(data, place.formatted_address
+            )
+    })  })
